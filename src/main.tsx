670591e5356584b7847145128cfe5578ce0b485a
Nikit Zykov <nikit@zykov.com>
2024-06-15 22:48:36 +0200

    Manifest added

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import App from './App.tsx'
 import './index.css'
 import { TonConnectUIProvider } from '@tonconnect/ui-react'
 
-const manifestUrl = 'https://raw.githubusercontent.com/ton-community/tutorials/main/03-client/test/public/tonconnect-manifest.json'
+const manifestUrl = 'https://cvdsfif.github.io/tonnerre-front/manifest.json'
 
 const container = document.getElementById("root")
 const root = createRoot(container!)
